75debd2dc93af1605a419580b6cdaf29c7b6f6b2
davidafcode <d.suarezafonso@gmail.com>
2026-03-04 19:44:02 +0000

    validación formulario contacto

--- com/example/service/ContactService.java
+++ com/example/service/ContactService.java
@@ -9,12 +9,15 @@ public class ContactService {
     public void processForm(ContactForm form) {
         Logger.log("Validando formulario...");
 
-        if (Validator.validateEmail(form.getEmail())) {
-            Logger.log("Email es válido!");
+        if (Validator.validateEmail(form.getEmail()) && Validator.validateName(form.getName())) {
+            Logger.log("Email y nombre válidos!");
+            Logger.log("Procesando formulario para: " + form.getName());
+            Logger.log("Mensaje enviado a: " + form.getName().toUpperCase());
+            Logger.log("Formulario procesado");
+        } else {
+            Logger.log("Nombre o email inválido! Vuelva a intentarlo.");
         }
 
-        Logger.log("Procesando formulario para: " + form.getName());
-        Logger.log("Mensaje enviado a: " + form.getName().toUpperCase());
-        Logger.log("Formulario procesado");
+
     }
 }
--- com/example/util/Validator.java
+++ com/example/util/Validator.java
@@ -5,4 +5,8 @@ public class Validator {
     public static boolean validateEmail(String email) {
         return email.contains("@");
     }
+
+    public static boolean validateName(String name) {
+        return !name.trim().isEmpty();
+    }
 }
